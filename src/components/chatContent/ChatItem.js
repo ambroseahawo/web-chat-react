40ced00b931dd228a544081e32d98050df20bcb2
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-09 07:49:24 +0300

    Implement chat message item as a reusable component

---
+++ src/components/chatContent/ChatItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ChatItem = (props) => {
+  return (
+    <div key={props.key} style={{ animationDelay: `0.5` }}
+      className={`chat__item ${props.user}`}
+    >
+      <div className="chat__item__content">
+        <div className="chat__meta">
+          <span style={{ textTransform: "capitalize" }}>{props.sender}</span>
+        </div>
+        <div className="chat__msg">{props.msg}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ChatItem
