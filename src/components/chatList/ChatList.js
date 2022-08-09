0621585baa7456d6f53fc5322d6b8c9ee295e4c9
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-09 07:26:35 +0300

    Implement chat list view

---
+++ src/components/chatList/ChatList.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./chatList.css";
+import ChatListItems from "./ChatListItem";
+
+const ChatList = () => {
+
+  const allChatUsers = [
+    {
+      image: "https://c0.wallpaperflare.com/preview/500/556/924/doberman-dog-beauty-nature.jpg",
+      id: 1,
+      name: "Group Chat",
+      active: true,
+      isOnline: true,
+    }
+  ]
+
+  return (
+    <div className="main__chatlist">
+      <div className="chatlist__heading">
+        <h2>Chat</h2>
+        <button className="btn-nobg">
+          <i className="fa fa-ellipsis-h"></i>
+        </button>
+      </div>
+      <div className="chatlist__items">
+        {allChatUsers.map((item, index) => {
+          return (
+            <ChatListItems
+              name={item.name}
+              key={item.id}
+              animationDelay={index + 1}
+              active={item.active ? "active" : ""}
+              isOnline={item.isOnline ? "active" : ""}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ChatList
