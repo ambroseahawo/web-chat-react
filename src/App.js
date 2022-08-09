5a26b3a1ef5a9d93c21618e78b28fc156a132194
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-09 07:34:07 +0300

    Implement chat list to the chat body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './components/nav/Nav'
+import ChatBody from './components/chatBody/ChatBody'
 import "./App.css"
 
 const App = () => {
   return (
     <div className="__main">
       <Nav />
+      <ChatBody />
     </div>
   )
 }
---
+++ src/components/chatBody/ChatBody.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChatList from "../chatList/ChatList"
+
+const ChatBody = () => {
+  return (
+    <div className="main__chatbody">
+      <ChatList />
+    </div>
+  )
+}
+
+export default ChatBody
