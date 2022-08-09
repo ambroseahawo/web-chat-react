4e0ec3e963cd212a387eb1bc63518cdac51c7905
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-09 08:21:07 +0300

    Refactor key prop on chat item component

--- src/components/chatContent/ChatItem.js
+++ src/components/chatContent/ChatItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ChatItem = (props) => {
   return (
-    <div key={props.key} style={{ animationDelay: `0.5` }}
+    <div style={{ animationDelay: `0.5` }}
       className={`chat__item ${props.user}`}
     >
       <div className="chat__item__content">
